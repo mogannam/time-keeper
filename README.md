b5c7e82ccb2b5af34f78c4f6b7ccbc34db6717ae
mogannam <JoeMogannam@gmail.com>
2021-10-24 15:05:13 -0700

    remove redudant code, add comments, clean up css

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Work Day Scheduler Starter Code
+
+# password-generator
+
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Mockup](#Mockup)
+
+## General info
+
+<p>A project that tracks daily tasks. The task is color coded to the hour, if the hour is in the past, present hour, or future hour. Task notes are saved to local storage, and the color coding runs every 5 minutes. </p>
+
+
+<a href=" https://github.com/mogannam/quizlet.git"> Project  on Github</a>
+
+<a href="https://mogannam.github.io/quizlet/"> Deployed Project</a>
+
+<p>Clone the projecct here: <em>git@github.com:mogannam/quizlet.git</em></p>
+	
+## Technologies
+Project is created with:
+* CSS
+* Java Script
+* HTML
+
+	
+## Setup
+Just open the index.html file in a web browser. This is just a simple html website that dynamically updates via Java Script.
+
+## Mockup
+<img src="https://github.com/mogannam/quizlet/blob/main/QuizletMockup.png?raw=true">
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,143 +1,137 @@
+// set the interval time to 5 minutes.
+//1,000 milliseconds by 60 to convert it to 1 minute. 
+//Then we multiply that minute by 5 to get a 5-minute timer.
+var intGlb_interval = 1000*60*5; // 5000
 
-//var EmptyBtn = $("<button>")
+// initialize a data structure for a task with some default values.
 var moment_todayAdjustHr = moment().hour(9).format("hha")
 moment_todayAdjustHr = moment().hour(9)
 var obj_aTask = { aMoment: moment_todayAdjustHr, index:-5,hour:9, hourStr:"09am",text : "Nothing to do :("}
 
+// an array of task objects that will be saved to local storage
 var arrObj_tasksSaved = []
 var date_rightNow = new Date();
 
-
+// global variables for adjusting css code easily
+var str_liCss = " list-group-item ";
+var str_liCssHidden = " list-group-item d-none "
+var str_textAreaCss = " task-cell col-9 "
+var str_divHourCss = " hour-cell col "
+var str_btnSaveCss = " button-cell col btn-info "
+var str_cssLstGrpItemDanger = " bg-danger "
+var str_cssLstGrpItemSuccess = " bg-success "
+var str_cssLstGrpItemSecond = " bg-secondary "
 
 var bool_dbg = false;
 
+//  get the dynamic section of the html page we will update
+// by injecting code onto it
 var ul_taskList = $("#time-block")
 
-var func_freshTaskList = ()=>{
-    if(bool_dbg)console.log("in func_freshTaskList ")
+// a function that either loads empty tasks, or pulls the tasks
+// saved in local storage and injects it onto the page
+var func_loadTaskList = ()=>{
+    
+    // an empty array of objects to hold what we pull from local storage
     var arrObj_temp = []
+    var arr_temp = JSON.parse(localStorage.getItem("arrObj_tasksSaved"));
     
-    
+    // loop thru all 24 hours in a day
     for (var index = 0; index < 24; index++) {
         
+        // if the hour is in a standard work day, add this code
+        // this structure allows us to add the rest of the hours later
+        // by adjusting the if statement.
         if(index >= 9 && index <= 17){
 
+            // get the time right now to build a data structure for each task
             date_rightNow = new Date();
             var hourRightNow = moment(date_rightNow, "L").format("hha")
             var moment_temp = moment(date_rightNow)
+
+            // get todays date, but adjust the hour to the ith hour
+            // starting at 9am, 10am, ..... ith, 4pm, 5pm
             moment_temp.set('hour', index)
             moment_temp.set('seconds',0)
             moment_temp.set('minute',0)
-             moment_temp.set('millisecond', 0);
+            moment_temp.set('millisecond', 0);
             
+            // create the task object data structure with default values
             obj_aTask = {aMoment:moment_temp, index:-1,hour:-1, hourStr:"-1am",text : "Nothing to do :("}
-            // for a  9 to 5 job, make one task for every hour
-            var int_tempHour = index
+            
+            // if there is cached data, load the cached data by overriding the default task.
+            if(arr_temp[index]) 
+                obj_aTask = arr_temp[index]
+
+            var int_tempHour = index // for the ith hour update its real hour
             obj_aTask.hour = int_tempHour;
-            // the hour starts at 9am, but after 12
-            // like a clock the hour starts at 1pm
+            
+            // add a string to the task object formatted as hourDigit, hourDigit, AM or PM
+            // i.e 09am, 12pm, 06am, 06pm
             obj_aTask.hourStr = moment_temp.format("hha")
-            obj_aTask.index = index
-             
-            var li_hourRow = $("<li>").addClass("list-group-item")
+            obj_aTask.index = index // update the task object with the correct index in the list
+            
+            // create a li element to inject onto the page
+            var li_hourRow = $("<li>").addClass(str_liCss)
             li_hourRow.attr('id', `hour-${index}-row`)
             
-            
-            var textArea_taskCell = $("<textarea>").addClass("task-cell col")
+            // create the task not input field
+            var textArea_taskCell = $("<textarea>").addClass(str_textAreaCss)
             textArea_taskCell.attr('id', `task-cell-${index}`)
-            textArea_taskCell.html(obj_aTask.text)
+            textArea_taskCell.html(obj_aTask.text) // set the text to what is cached or default text
         
-            
-            var Div_hourCell = $("<div>").addClass("hour-cell col")
+            // create the hour cell to inject on the page
+            var Div_hourCell = $("<div>").addClass(str_divHourCss)
             Div_hourCell.attr('id', `hour-cell-${index}`)
-            Div_hourCell.html(obj_aTask.hourStr)
+            Div_hourCell.html(obj_aTask.hourStr) // set the text value to its correct ith hour
 
-            var btn_save = $("<button>").addClass("button-cell col")
+            // create the uique button to save the data when the user edits the task note.
+            var btn_save = $("<button>").addClass(str_btnSaveCss)
             btn_save.attr('id', `btn-cell-${index}`)
             btn_save.html('Save')
 
+            // create a empty div, so that we can use bootstrap to format the row
             var div_emptyContainer = $("<div>").addClass("row")
+            //add our new elements to the empty div, filling it
             div_emptyContainer.append(Div_hourCell)
             div_emptyContainer.append(textArea_taskCell)
             div_emptyContainer.append(btn_save)
+            // add the div to the li
             li_hourRow.append(div_emptyContainer)
+            // add the li to the unorderred list that is already on the page
             ul_taskList.append(li_hourRow);
-            //arrObj_temp.push(obj_aTask)
-
-            // console.log(index)
-            // console.log(obj_aTask)
             
+            //cache the data, we pulled from local storage into the global variable
+            // this really only has a significant effect the first time the page is opened.
             arrObj_tasksSaved[index] = obj_aTask;
+
+            //setting the arrObj_temp to this value is needed for the code to run properly
+            // without this line. Sometimes when the page loads bada data is still saved in arrObj_temp
             arrObj_temp[index]= arrObj_tasksSaved[index]
-        
+            
+            // cache the data in local storage. This only has high significance 
+            // when the page is run for the very first time
             localStorage.setItem("arrObj_tasksSaved", JSON.stringify(arrObj_tasksSaved)); 
         }
         else{
-            
-            var li_hourRow = $("<li>").addClass(" list-group-item d-none")
+            // else we are not tracking the ith hour in the day.
+            // so create an empty li, and hide it. This is needed, for the code to
+            // execute properly. Without it we can't use jquery to grab the closest li
+            // by its index on index.html.
+            var li_hourRow = $("<li>").addClass(str_liCssHidden)
             li_hourRow.attr('id',`hour-${index}-row`)
             ul_taskList.append(li_hourRow);
         }
            
     }
-    var moment_todayAdjustHr = moment().hour(14)
-    console.log(moment_todayAdjustHr)
     
     
     
-}
-
-var func_loadCachTask= ()=>{
-    if(bool_dbg)console.log('in func_loadCachTask')
-    //get cached task
-    var arr_temp = JSON.parse(localStorage.getItem("arrObj_tasksSaved"));
-    for(var index = 0; index < arr_temp.length; index++) {
-    // beg foreach loop
-    var element = arr_temp[index]
-    if(element){ 
-        // The calendar only runs from 9am to 5pm, if other hours are null do nothing
-        // if the hours are not null, i.e the element is full execute code
-        var li_hourRow = $("<li>").addClass(" list-group-item")
-        li_hourRow.attr('id',`hour-${element.index}-row`)
-            
-        var textArea_taskCell = $("<textarea>").addClass("task-cell col")
-            textArea_taskCell.attr('id', `task-cell-${index}`)
-           
-            textArea_taskCell.html(element.text)
-        
-            
-            var Div_hourCell = $("<div>").addClass("hour-cell col")
-            Div_hourCell.attr('id', `hour-cell-${index}`)
-            Div_hourCell.html(element.hourStr)
-
-            var btn_save = $("<button>").addClass("button-cell col")
-            btn_save.attr('id', `btn-cell-${index}`)
-            btn_save.html('Save')
-
-            var div_emptyContainer = $("<div>").addClass("row")
-            div_emptyContainer.append(Div_hourCell)
-            div_emptyContainer.append(textArea_taskCell)
-            div_emptyContainer.append(btn_save)
-            li_hourRow.append(div_emptyContainer)
-            ul_taskList.append(li_hourRow);
-            arrObj_tasksSaved[element.index] = arr_temp[element.index]
-        }
-        else{
-            
-            
-            var li_hourRow = $("<li>").addClass("list-group-item d-none")
-            li_hourRow.attr('id',`hour-${index}-row`)
-            ul_taskList.append(li_hourRow);
-            
-        }
-    
-    } // end for each loop
-    localStorage.setItem("arrObj_tasksSaved", JSON.stringify(arrObj_tasksSaved));
-
     
 }
 
 var func_TodaysDate = ()=>{
+    // load todays date onto the page dynamically
     var date_rightNowTemp = new Date();
     var time = moment(date_rightNowTemp, "L").format("dddd, MMMM, Do");
     
@@ -145,6 +139,11 @@ var func_TodaysDate = ()=>{
 }
 
 var func_auditTime = ()=>{
+    // based on the global variable. AUdit the task hour and update the color coding.
+    // the default is to check every 5 minutes.
+
+    // get the time right now, and zero out the seconds, minutes, milleseconds
+    // we only care about the hour
     date_rightNow = new Date();
     var moment_todayAdjustHr = moment().hour(14).format("hha")
     var hourRightNow = moment(date_rightNow, "L").format("hha")
@@ -152,61 +151,56 @@ var func_auditTime = ()=>{
     momentRightNow.set('seconds',0)
     momentRightNow.set('minute',0)
     
-    //if(bool_dbg)console.log(arrObj_tasksSaved)
+    // for evvery ith li item on the page, audit the li.
     arrObj_tasksSaved.forEach(element => {
+        // reformat that time saved in our object, as a Moment data structure
         var tempMoment = moment(element.aMoment)
+
+        // test the current time against our saved time stamp to see if
+        // before -> gray
+        // during -> red
+        // after -> green
         var bool_isAfter = tempMoment.isAfter(momentRightNow, 'hour');
         var bool_isBefore = tempMoment.isBefore(momentRightNow, 'hour');
         var bool_isSame = tempMoment.isSame(momentRightNow, 'hour');
         
         var index = element.index
-        
+        //delete any old css code
+        $(`#task-cell-${index}`).removeClass()
         if(element) // if element is not null
-            if(bool_isSame)
-                $(`#hour-${index}-row`).addClass("list-group-item-danger")
-            if(bool_isAfter)
-                $(`#hour-${index}-row`).addClass("list-group-item-success")
-            if(bool_isBefore)
-                $(`#hour-${index}-row`).addClass("list-group-item-secondary")
+            if(bool_isSame) // if you are in the present, update css code to red
+                $(`#task-cell-${index}`).addClass(str_cssLstGrpItemDanger+str_textAreaCss)
+            if(bool_isAfter)// if you are in the future, update css code
+                $(`#task-cell-${index}`).addClass(str_cssLstGrpItemSuccess+str_textAreaCss)
+            if(bool_isBefore)// if you are in the past update css code
+                $(`#task-cell-${index}`).addClass(str_cssLstGrpItemSecond+str_textAreaCss)
             
         
     });
 }
 
+// call helper functionns to dynamically update page
 var func_loadTasks = ()=>{
     func_TodaysDate()
-
-    if(bool_dbg)console.log("in func_loadTasks")
-    // .ready waits until the page is ready for manipulation
-    // then this function can execute
-    // uses a Java sccript 6 short hand arrow function to save on writing code
-    var arr_temp = JSON.parse(localStorage.getItem("arrObj_tasksSaved"));
-    
-    if(!arr_temp)func_freshTaskList();
-    else{
-        func_loadCachTask()
-    }
-    
-    
-
-
-    
+    func_loadTaskList();
 
 };
 
+// code used to save user input to page
 var func_saveTask = (index)=>{
     var str_tempText = $(`#task-cell-${index}`).val()
     
-    if(bool_dbg)console.log(`saving ${index} ${str_tempText}`)
     JSON.parse(localStorage.getItem("arrObj_tasksSaved"));
     
     arrObj_tasksSaved[index].text = str_tempText
+
     if(bool_dbg)console.log(arrObj_tasksSaved[index])
     localStorage.setItem("arrObj_tasksSaved", JSON.stringify(arrObj_tasksSaved));
 
 
 }
 
+// add event listiner to the button, to save user data
 $(".list-group").on("click", "button",
     ()=>{
         var index = "Null"
@@ -221,21 +215,20 @@ $(".list-group").on("click", "button",
     }
 )
 
+// set a timer and execute the audit functionby the defined time interval
+// the default interval was 5 minutes
 var func_startInterval = ()=>{
     
     setInterval(
         ()=>{
-            console.log("counting---------------")
+            //console.log("counting---------------")
             func_auditTime();
         },
-        5000);
+        intGlb_interval);
    
 }
 
-
-
-
 func_loadTasks();
 func_auditTime();
-//func_startInterval();
+func_startInterval();
 
